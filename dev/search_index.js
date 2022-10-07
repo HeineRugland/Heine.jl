c1a0eb059c8b2422508fe23fa5a8fa1770e3378e
Documenter.jl <documenter@juliadocs.github.io>
2022-10-07 12:55:13 +0000

    build based on cb0a78c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Heine","category":"page"},{"location":"#Heine","page":"Home","title":"Heine","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Heine.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Heine]","category":"page"}]
+}
