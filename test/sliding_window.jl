2837e727923051b5d879d1a7d6d65d06c7695dfc
Heine Rugland <heine.rugland@gmail.com>
2022-10-14 15:25:29 +0200

    Added documentation, tests and revised some code.

--- test/sliding_window.jl
+++ test/sliding_window.jl
@@ -9,4 +9,14 @@ x = [1,2,3,4,5,6,7,8,9]
 y = [1,2,3,4,5,6,7,8,9]
 expected_x = [2,3,4,5,6,7,8]
 expected_y = [2,3,4,5,6,7,8]
-@test sliding_window(x,y; f = mean, width = 3, step = 1) == (expected_x, expected_y)
+@test sliding_window(x, y; f = mean, width = 3, step = 1) == (expected_x, expected_y)
+
+x = [1,2,3,4]
+y = [5,6,7,8]
+
+@test npt_running_average(x, y; n = 3) == ([2, 3], [7.0, 8.0])
+
+x = [1,2,3,4,5,6,7,8,9]
+y = [1,2,3,4,5,6,7,8,9]
+
+@test npt_running_average(x, y) == ([3, 4, 5, 6, 7], [3.0, 4.0, 5.0, 6.0, 7.0])
